734dcaf50385b33912dcdb983ed107a7dabaa457
Satya Prakash <satyaprakash99915@gmail.com>
2024-04-02 12:42:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Rename and move Shodanizer.sh file to /usr/local/bin/shodanizer
-sudo mv Shodanizer.sh /usr/local/bin/shodanizer
+# Rename and move Shodanizer.sh file to /usr/bin/shodanizer
+sudo mv Shodanizer.sh /usr/bin/shodanizer
 
 # Make the Shodanizer file executable
-sudo chmod u+x /usr/local/bin/shodanizer
+sudo chmod u+x /usr/bin/shodanizer
 
 # Remove the Shodanizer folder from the home directory
 if [ -d "$home_dir/Shodanizer" ]; then
